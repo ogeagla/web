7be720ba79610e292b83dc88fc7751712ec718d5
Octavian Geagla <ogeagla@gmail.com>
2012-12-15 21:16:32 -0500

    Eye effect work

--- js/playground.js
+++ js/playground.js
@@ -1,8 +1,60 @@
+//can use this for face distorition (load img as rgb matrix):
+//http://beej.us/blog/data/html5s-canvas-2-pixel/
+
+var colors = new Array();
+colors[0] = '#5a554d';
+colors[1] = '#5a4c36';
+colors[2] = '#747474';
+colors[3] = '#7E6969';
+colors[4] = '#8E6262';
+colors[5] = '#9C6161';
+colors[6] = '#A85C5C';
+colors[7] = '#FF0000';
+
+var playgroundCanvas = '';//= document.getElementById('playgroundCanvas');
+var ctx = '';
+
+
+var canvasWidth = '';//playgroundCanvas.width;//600;
+var canvasHeight = '';//playgroundCanvas.height;//800;
+
+var leftEyeXRatio = 0.33;
+var leftEyeYRatio = 0.4183;
+var rightEyeXRatio = 0.694;
+var rightEyeYRatio = 0.421;
+var eyeRadius = 2.8;
+
+var dColorIndex = -1;
+var colorIndex = 0;
+
+var eyesIntervalHandle = '';
+
+var face = new Image();
+face.src = 'media/img/meface.JPG';
+
+
+
+/*
+var centerXLeft = 0.33;
+var centerYLeft = 0.4183;
+var centerXRight = 0.694;
+var centerYRight = 0.421;
+var radius = 1.6;
+
+var di = -1;
+var colorIndex = 0;
+
+var h = '';
+*/
+
 $(document).ready(function(){
 
-    var canvas = document.getElementById('tutorial');
-    if (canvas.getContext){
-	var ctx = canvas.getContext('2d');
+    playgroundCanvas = document.getElementById('playgroundCanvas');
+    canvasWidth = playgroundCanvas.width;//600;
+		canvasHeight = playgroundCanvas.height;//800;
+
+    if (playgroundCanvas.getContext){
+		ctx = playgroundCanvas.getContext('2d');
   	drawStuff();
 
   	
@@ -15,72 +67,41 @@ $(document).ready(function(){
 
 function drawFace() {
 	
-	 var ctx = document.getElementById('tutorial').getContext('2d');
-    var face = new Image();
-    face.src = 'media/img/meface.JPG';
-
-
-    ctx.drawImage(face, 0, 0, faceWidth, faceHeight);
+    ctx.drawImage(face, 0, 0, canvasWidth, canvasHeight);
 }
 function drawStuff() {
 
-
-    
+	    
     drawFace();
-    h = eyesTrigger();
-
-    
+    eyesIntervalHandle = eyesTrigger();
+ 
 
 }
-var colors = new Array();
-colors[0] = '#5a554d';
-colors[1] = '#5a4c36';
-colors[2] = '#747474';
-colors[3] = '#7E6969';
-colors[4] = '#8E6262';
-colors[5] = '#9C6161';
-colors[6] = '#A85C5C';
-colors[7] = '#FF0000';
 
-
-var centerXLeft = 0.33;
-var centerYLeft = 0.4183;
-var centerXRight = 0.694;
-var centerYRight = 0.421;
-var radius = 1.8;
-
-var faceWidth = 200;
-var faceHeight = 300;
-var di = -1;
-var colorIndex = 0;
-
-var h = '';
 
 function eyesTrigger() {
-	return setInterval(drawEyes, 100);
+	return setInterval(drawEyes, 70);
 }
     
 function drawEyes() {
 	
-	var ctx = document.getElementById('tutorial').getContext('2d');
-
 	
 	if(colorIndex == 0) {
 		ctx.globalAlpha = 1.;
 		
-		di = di*-1;
+		dColorIndex = dColorIndex * -1.;
 		
 	}
 	if(colorIndex == colors.length - 2){
-		di = di*-1;
+		dColorIndex = dColorIndex*-1.;
  	}	
- 	ctx.globalAlpha = 0.22;
+ 	ctx.globalAlpha = 0.52;
  	//console.log(colorIndex);
  	drawFace();
-	drawCircle(ctx, centerXLeft*faceWidth, centerYLeft*faceHeight, radius, colors[colorIndex], colors[colorIndex]);
-   drawCircle(ctx, centerXRight*faceWidth, centerYRight*faceHeight, radius, colors[colorIndex],colors[colorIndex]);
+	drawCircle(ctx, leftEyeXRatio*canvasWidth, leftEyeYRatio*canvasHeight, eyeRadius, colors[colorIndex], colors[colorIndex]);
+  drawCircle(ctx, rightEyeXRatio*canvasWidth, rightEyeYRatio*canvasHeight, eyeRadius, colors[colorIndex],colors[colorIndex]);
    
-   colorIndex = colorIndex + di;
+  colorIndex = colorIndex + dColorIndex;
 }
 
 function drawCircle(ctx, x, y, r, fillColor, strokeColor) {
@@ -92,6 +113,8 @@ function drawCircle(ctx, x, y, r, fillColor, strokeColor) {
     ctx.lineWidth = 0;
     ctx.strokeStyle = strokeColor;//#003300
     ctx.stroke();
+    
+    console.log("drawing ciclr");
 }
 
 
