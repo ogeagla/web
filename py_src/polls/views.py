3ef9d7a697e1ef12f865502b982639ae1b6719a4
Octavian Geagla <ogeagla@gmail.com>
2013-03-16 00:48:03 -0400

    Tutorial work.

--- py_src/polls/views.py
+++ py_src/polls/views.py
@@ -1,10 +1,42 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = Context({
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+
+'''
+alternate, better implementation:
+
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+'''
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+'''alternate, better implementation:
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+'''
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
